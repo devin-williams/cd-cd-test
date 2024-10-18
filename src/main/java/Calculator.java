1998949ea90629aa39051e23c9c7309c577842d8
Devin Williams <dcw003@byu.edu>
2024-10-18 12:20:22 -0600

    added the methods

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.UUID;
+
 // Create a class called Calculator
 // This class should have the following methods
 class Calculator {
@@ -39,7 +41,16 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 
 
@@ -51,7 +62,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int number){
-        return null;
+        return Integer.toBinaryString(number);
     }
 
     /*
@@ -63,7 +74,7 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-        return null;
+        return n + UUID.randomUUID();
     }
 
 
